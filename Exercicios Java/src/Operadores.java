12186777f1e4bcd5da65b62a37152a204f742f66
Jacques Douglas <jacques.ti@hotmail.com>
2022-11-08 22:56:35 -0300

    Criando o Projeto/ Exercicios Java

---
+++ Exercicios Java/src/Operadores.java
@@ -0,0 +1,55 @@
+public class Operadores {
+    
+    public static void main(String[] args) {
+        
+        // OPERADORES E CONCATENAÇÃO
+
+        /*  String concatenacao = "?";
+            System.out.println(concatenacao);
+        concatenacao = 1+1+1+"1";
+            System.out.println(concatenacao);
+        concatenacao = 1+"1"+1+1;
+            System.out.println(concatenacao);
+         concatenacao = 1+"1"+1+"1";
+            System.out.println(concatenacao);
+        concatenacao = "1"+(1+1+1);
+            System.out.println(concatenacao); */
+
+        // OPERADORES UNARIOS 
+
+       /*  int numero = 5;
+        numero = - numero;
+
+        System.out.println(numero); */
+
+        // INCREMENTAÇÃO
+
+        //int numero = 5;
+
+        //x repetição
+
+        //numero = numero +1
+
+        /* System.out.println(++numero);
+        System.out.println(numero); */
+
+        // NEGAÇÃO DE VARIAVEL BOOLEANA
+
+        /* boolean variavel = true;
+        variavel = !variavel;
+        System.out.println(variavel); */
+
+        
+        
+
+
+
+
+
+        
+
+        
+        
+    }
+}
+ 
---
+++ Exercicios Java/src/OperadoresTernarios.java
@@ -0,0 +1,19 @@
+public class OperadoresTernarios {
+    
+
+    public static void main(String[] args) {
+
+        // OPERADORES TERNARIOS
+        
+        int a,b;
+        a = 5;
+        b = 6;
+        int resultado = a==b ? 1 : 0;
+        
+        System.out.println(resultado);
+
+        
+
+        
+    }
+}
